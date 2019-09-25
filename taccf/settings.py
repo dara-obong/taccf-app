ec07e6b7fcf7e7101c16c016be676b8434b09ab6
unknown <teyo2012@gmail.com>
2019-09-25 20:45:17 +0100

    Updated settings

--- taccf/settings.py
+++ taccf/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3^^b=mu6_c^sxm!ap)az3a0kt&w-1-=(*bus5io-%s0(903p0_'
+#SECRET_KEY = '3^^b=mu6_c^sxm!ap)az3a0kt&w-1-=(*bus5io-%s0(903p0_'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taccfapp.herokuapp.com']
 
 
 
