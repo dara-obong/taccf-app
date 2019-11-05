723aed977c6faa00b5443ab5a679d9888b6bb6b0
Theodore Eyo <teyo2012@gmail.com>
2019-11-05 01:48:23 +0100

    reversed all changes

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,9 @@ def about(request):
     return render(request, 'blog/about.html', {'title': 'About Us'})
 
 
+def contact(request):
+    return render(request, 'blog/contact.html', {'title': 'Contact Us'})
+
 def post(request):
     context = {
         'posts': Post.objects.all()
